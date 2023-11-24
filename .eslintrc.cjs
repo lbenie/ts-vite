03ff769d22837d0a8ddecc66770ef4884fb82880
Lucien Bénié <lucien.benie@gmail.com>
2023-11-24 02:17:48 +0000

    chore: use my linter rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,3 @@
-/* eslint-disable functional/immutable-data */
-/**
- * @type {import('eslint').ESLint.Options}
- */
 module.exports = {
-  root: true,
-  extends: [
-    'eslint:recommended',
-    'prettier',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:functional/no-mutations',
-  ],
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    project: ['./tsconfig.json', './tsconfig.spec.json'],
-    sourceType: 'module',
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['prettier', '@typescript-eslint', 'functional'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      { singleQuote: true, semi: false, trailingComma: 'all' },
-    ],
-    '@typescript-eslint/consistent-type-imports': ['error'],
-    '@typescript-eslint/prefer-readonly': ['error'],
-    'functional/prefer-tacit': ['error'],
-  },
-  env: {
-    browser: true,
-    node: true,
-  },
-}
+  extends: [require.resolve('@lbenie/linting/eslint')],
+};
